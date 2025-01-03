98d9f5d0ba0256e1a0bd9b2b183368a58b015bb1
X6oPk4i9w <junxilu2021@gmail.com>
2025-01-03 23:09:06 +0800

    Update https.sh

--- https.sh
+++ https.sh
@@ -1,43 +1,43 @@
 #!/bin/sh
 # forum: https://1024.day
-
 if [[ $EUID -ne 0 ]]; then
-    clear
-    echo "Error: This script must be run as root!" 1>&2
-    exit 1
+   clear
+   echo "Error: This script must be run as root!" 1>&2
+   exit 1
 fi
 
 timedatectl set-timezone Asia/Shanghai
-Passwd=$(cat /dev/urandom | head -1 | md5sum | head -c 12)
+
+# 生成随机用户名和密码
+username=$(echo -n $(cat /dev/urandom | tr -dc 'a-zA-Z' | head -c 1)$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 9))
+password=$(echo -n $(cat /dev/urandom | tr -dc 'a-zA-Z' | head -c 1)$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 14))
 
 wget https://github.com/yeahwu/image/raw/refs/heads/master/caddy.tar.gz -O - | tar -xz -C /usr/local/
-
 echo "====输入已经DNS解析好的域名===="
 read domain
 
-    isPort=`netstat -ntlp| grep -E ':80 |:443 '`
-    if [ "$isPort" != "" ];then
-        clear
-        echo " ================================================== "
-        echo " 80或443端口被占用，请先释放端口再运行此脚本"
-        echo
-        echo " 端口占用信息如下："
-        echo $isPort
-        echo " ================================================== "
-        exit 1
-    fi
+isPort=`netstat -ntlp| grep -E ':80 |:443 '`
+if [ "$isPort" != "" ];then
+   clear
+   echo " ================================================== "
+   echo " 80或443端口被占用，请先释放端口再运行此脚本"
+   echo
+   echo " 端口占用信息如下："
+   echo $isPort
+   echo " ================================================== "
+   exit 1
+fi
 
 mkdir -p /etc/caddy
-
 cat >/etc/caddy/https.caddyfile<<EOF
 :443, $domain
 route {
-	forward_proxy {
-		basic_auth 1024 $Passwd
-		hide_ip
-		hide_via
-	}
-	file_server
+   forward_proxy {
+       basic_auth $username $password
+       hide_ip
+       hide_via
+   }
+   file_server
 }
 EOF
 
@@ -47,7 +47,6 @@ Description=Caddy
 Documentation=https://caddyserver.com/docs/
 After=network.target network-online.target
 Requires=network-online.target
-
 [Service]
 User=root
 ExecStart=/usr/local/caddy run --environ --config /etc/caddy/https.caddyfile
@@ -57,7 +56,6 @@ LimitNOFILE=1048576
 PrivateTmp=true
 ProtectSystem=full
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -70,24 +68,23 @@ cat >/etc/caddy/https.json<<EOF
 ===========配置参数=============
 代理模式：Https正向代理
 地址：${domain}
-端口：60375
-用户：zU3jozq0B0dscQAvq
-密码：${Passwd}
+端口：443
+用户：${username}
+密码：${password}
 ====================================
-http=$domain:443, username=1024, password=$Passwd, over-tls=true, tls-verification=true, tls-host=$domain, udp-relay=false, tls13=true, tag=https
+http=$domain:443, username=$username, password=$password, over-tls=true, tls-verification=true, tls-host=$domain, udp-relay=false, tls13=true, tag=https
 }
 EOF
 
-    echo
-    echo "安装已经完成"
-    echo
-    echo "===========Https配置参数============"
-    echo
-    echo "地址：${domain}"
-    echo "端口：443"
-    echo "密码：${Passwd}"
-    echo "用户：1024"
-    echo
-    echo "========================================="
-    echo "http=$domain:443, username=1024, password=$Passwd, over-tls=true, tls-verification=true, tls-host=$domain, udp-relay=false, tls13=true, tag=https"
-    echo
+echo
+echo "安装已经完成"
+echo
+echo "===========Https配置参数============"
+echo
+echo "地址：${domain}"
+echo "端口：443"
+echo "用户：${username}"  
+echo "密码：${password}"
+echo
+echo "========================================="
+echo "http=$domain:443, username=$username, password=$password, over-tls=true, tls-verification=true, tls-host=$domain, udp-relay=false, tls13=true, tag=https"
